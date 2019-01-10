fd3a579078ff2389bb287f06b1f1bb5e9cca433b
M. Rizky Hidayat <emrizkyha@gmail.com>
2019-01-10 13:06:07 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Smesa Hotel - SMK Negeri 1 Surabaya</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="styles/bootstrap.min.css">
+    <!-- Animate -->
+    <link rel="stylesheet" href="styles/animate.css">
+    <!-- Venobox -->
+    <link rel="stylesheet" href="styles/venobox.css">
+
+    <!-- fonts -->
+    <link rel="stylesheet" href="fonts/font-awesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/css/flaticon.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,400i,500,600,700,800,900" rel="stylesheet">
+    <!-- Main Css -->
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+<script src="scripts/jquery-3.3.1.min.js"></script>
+<script src="scripts/popper.min.js"></script>
+<script src="scripts/bootstrap.min.js"></script>
+<script src="scripts/jquery.easing.min.js"></script>
+<script src="scripts/wow.js"></script>
+<script src="scripts/venobox.min.js"></script>
+<script src="scripts/SmoothScroll.js"></script>
+<script src="scripts/jquery.filterizr.min.js"></script>
+<script src="scripts/sticky-kit.min.js"></script>
+<script src="scripts/form-validator.min.js"></script>
+<script src="scripts/contact-form-script.js"></script>
+<script src="scripts/script.js"></script>
+</body>
+</html>
